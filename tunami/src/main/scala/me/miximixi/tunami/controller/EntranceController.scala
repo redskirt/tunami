ce6d36d37ac2080eb8300eccc18bccf73072ae5c
Sasaki <redskirt@outlook.com>
2018-06-01 00:08:05 +0800

    Map of index page make completely.

--- tunami/src/main/scala/me/miximixi/tunami/controller/EntranceController.scala
+++ tunami/src/main/scala/me/miximixi/tunami/controller/EntranceController.scala
@@ -1,10 +1,8 @@
 package me.miximixi.tunami.controller
 
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.{RequestMapping, RequestMethod, RestController}
 import org.springframework.beans.factory.annotation.Autowired
 import me.miximixi.tunami.service.LoginService
-import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.servlet.ModelAndView
 
 /**
@@ -13,14 +11,16 @@ import org.springframework.web.servlet.ModelAndView
  * @Timestamp May 30, 2018 4:58:47 PM
  * @Description
  */
-
 @RestController
-class MainController {
+class EntranceController {
 
   @Autowired
   var loginService: LoginService = _
 
   @RequestMapping(value = { Array("/", "/login", "/home") }, method = Array(RequestMethod.GET))
-  def /() = new ModelAndView("login")
+  def / = new ModelAndView("login")
+
+  @RequestMapping(value = { Array("/_") }, method = Array(RequestMethod.GET))
+  def index = new ModelAndView("index")
 
 }
